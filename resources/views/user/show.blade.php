043510aaaf8396282c9c091a6d59f01347a65e85
James Yu <jamesyu@Jamesde-MacBook-Air.local>
2018-08-15 14:41:48 +0800

    Add show detail.

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -4,30 +4,8 @@
         <h1 class="text-center">{{$user->name}}</h1>
     </div>
     <div class="card-body">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td scope="row"></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td scope="row"></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="card-footer text-muted">
-        Footer
+        <label>Email</label>
+        <h3>{{$user->email}}</h3>
     </div>
 </div>
 @endsection
